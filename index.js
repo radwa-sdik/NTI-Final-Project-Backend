5283e42a66d6722815eaeb6196446f146e10616d
Radwa Mohamed <radwasedeek2013@gmail.com>
2026-01-16 23:31:57 +0200

    update accepted payload size

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ mongoose.connect(process.env.DB_URL)
 
 app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 const server = http.createServer(app);
 const io = socketIo(server, {
